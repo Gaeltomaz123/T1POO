082635b076183207070d808895223010fb593a34
Bruno Neves <brunobealnev@Gmail.com>
2023-09-01 19:02:47 -0300

    Adicionando comentários

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,18 +8,22 @@ public class App
         Vagao vagaoUm = new Vagao(0, 0, null);
         Composicao composicaoUm = new Composicao(1, 0, null, 0, null);
         
+        //ENGATANDO LOCOMOTIVA
         System.out.println("Antes: "+composicaoUm.getLocomotiva());
         composicaoUm.engataLocomotiva(locomotivaUm);
         System.out.println("Depois: "+ composicaoUm.getLocomotiva());
 
+        //ENGATANDO VAGÃO
         System.out.println("Antes: "+composicaoUm.getVagao());
         composicaoUm.engataVagao(vagaoUm);
         System.out.println("Depois: "+ composicaoUm.getVagao());
 
+        //DESENGATANDO LOCOMOTIVA
         System.out.println("Antes: "+composicaoUm.getLocomotiva());
         composicaoUm.desengataLocomotiva(locomotivaUm);
         System.out.println("Depois: "+ composicaoUm.getLocomotiva());
 
+        //DESENGATANDO VAGÃO
         System.out.println("Antes: "+composicaoUm.getVagao());
         composicaoUm.desengataVagao(vagaoUm);
         System.out.println("Depois: "+ composicaoUm.getVagao());
