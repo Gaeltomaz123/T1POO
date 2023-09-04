eb134dcd627c1e6d927d4383b8f22ff63681239e
Bruno Neves <brunobealnev@Gmail.com>
2023-09-04 19:01:49 -0300

    Resolvendo erro

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,7 +6,7 @@ public class App {
         Vagao[] arrayVagao = new Vagao[5];
         Composicao[] arrayComposicao = new Composicao[5];
 
-        arrayComposicao[0] = new Composicao(0, 0, null, 0, null);
+        arrayComposicao[0] = new Composicao("1", 0, null, 0, null);
         arrayLocomotivas[0] = new Locomotiva(0, 0, 0, null);
         arrayVagao[0] = new Vagao(0, 0, null);
 
